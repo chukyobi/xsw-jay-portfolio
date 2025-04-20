02c9f4a5561422534e11a8010c889949dbdf2e61
Chukwudi Obi <162724360+chukyobi@users.noreply.github.com>
2025-04-20 16:33:52 -0700

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Github, Linkedin, Twitter, MousePointerClick, Sparkles } from "lucide-react";
 import Image from "next/image";
