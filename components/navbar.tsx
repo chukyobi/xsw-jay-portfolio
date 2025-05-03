b03e8aaee0827b34b1be5190df6b7e1e46aca1fe
Chukwudi Obi <162724360+chukyobi@users.noreply.github.com>
2025-05-03 03:32:47 -0700

    fix: navbar orientation

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,39 +1,67 @@
-"use client"
+"use client";
 
-import { Home, Twitter, Instagram, Mail, FileText, Github, BriefcaseBusiness  } from "lucide-react"
-import Link from "next/link"
+import {
+  Home,
+  Twitter,
+  Instagram,
+  Mail,
+  FileText,
+  Github,
+  BriefcaseBusiness,
+} from "lucide-react";
+import Link from "next/link";
 
 export function Navbar() {
   return (
-    <div className="fixed z-50 flex items-center space-x-4 px-6 py-2 bg-white border rounded-full shadow-md
+    <div
+      className="fixed z-50 flex items-center space-x-4 px-6 py-2 bg-white border rounded-full shadow-md
                     left-1/2 -translate-x-1/2 
-                    top-6 md:top-6 bottom-auto
-                    md:bottom-auto md:top-6
-                    sm:bottom-6 sm:top-auto">
+                    bottom-6 top-auto
+                    md:bottom-6 md:top-auto
+                    sm:bottom-6 sm:top-auto xs:bottom-6 xs:top-auto"
+    >
       {/* Icons */}
-      <Link href="/" className="text-muted-foreground hover:text-foreground">
+      <Link href="/" className="text-muted-foreground hover:text-yellow-500">
         <Home className="w-5 h-5" />
       </Link>
       <div className="h-5 w-px bg-muted" />
-      <Link href="https://twitter.com/chukyobi_" target="_blank" className="text-muted-foreground hover:text-foreground">
-        <BriefcaseBusiness className="w-5 h-5" />
-      </Link>
-      <Link href="https://instagram.com/chuky.obi_" target="_blank" className="text-muted-foreground hover:text-foreground">
-        <Mail className="w-5 h-5" />
-      </Link>
-      <Link href="/resume.pdf" target="_blank" className="text-muted-foreground hover:text-foreground">
-        <FileText className="w-5 h-5" />
-      </Link>
+
+      <div className="relative group flex flex-col items-center">
+        <Link
+          href="mailto:josephclinton.obi@gmail.com"
+          className="text-muted-foreground hover:text-green-500 transition-all duration-300 transform group-hover:-translate-y-1"
+        >
+          <Mail className="w-5 h-5" />
+        </Link>
+        <span className="absolute top-3 text-xs text-muted-foreground opacity-0 translate-y-1 transition-all duration-300 group-hover:opacity-100 group-hover:translate-y-0">
+          mail
+        </span>
+      </div>
+
+      <div className="relative group flex flex-col items-center">
+        <Link
+          href="https://docs.google.com/document/d/1qsa6TBTrUexoVbwmaJezEkeyJgeJAfuHX8A7YpCaBbI/edit?usp=sharing"
+          target="_blank"
+          className="text-muted-foreground hover:text-blue-500 transition-all duration-300 transform group-hover:-translate-y-1"
+        >
+          <FileText className="w-5 h-5" />
+        </Link>
+        <span className="absolute top-3 text-xs text-muted-foreground opacity-0 translate-y-1 transition-all duration-300 group-hover:opacity-100 group-hover:translate-y-0">
+          cv
+        </span>
+      </div>
+
       <div className="h-5 w-px bg-muted" />
-      
+
       {/* Github Button */}
       <Link
         href="https://github.com/chukyobi"
         target="_blank"
-        className="bg-black text-white text-sm font-medium px-4 py-2 rounded-full ml-2"
+        className=" ml-2"
       >
-        <Github className="w-5 h-5" />
+        {/* <Github className="w-5 h-5" /> */}
+        <img src="/icons8-github.svg" alt="github-logo" className="w-7 h-7" />
       </Link>
     </div>
-  )
+  );
 }
