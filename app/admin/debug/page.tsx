731fde235c47e39420dd57b86540b5aeb6208aeb
Chukwudi Obi <162724360+chukyobi@users.noreply.github.com>
2025-05-03 10:19:03 -0700

    Update page.tsx

--- app/admin/debug/page.tsx
+++ app/admin/debug/page.tsx
@@ -1,4 +1,5 @@
-// "use client"
+//"use client"
+//
 
 // import { useState, useEffect } from "react"
 // import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
